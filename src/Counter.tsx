76e82f4fcd9426d0e7020f19b6690caf9c13f6cc
Vijay T <vijayst@gmail.com>
2021-08-02 18:00:02 +0530

    set storybook

--- src/Counter.tsx
+++ src/Counter.tsx
@@ -1,13 +1,16 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Button from "./Button";
 import Count from "./Count";
 
-interface CounterProps {
+export interface CounterProps {
   defaultCount?: number;
 }
 
 const Counter: React.FC<CounterProps> = ({ defaultCount = 0 }) => {
   const [count, setCount] = useState<number>(defaultCount);
+  useEffect(() => {
+    setCount(defaultCount);
+  }, [defaultCount]);
   function handleClick() {
     setCount((count) => count + 1);
   }
